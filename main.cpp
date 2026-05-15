3760f8223edfa1f2449e1a58716b3630f4ee2632
Theshakyhorse <amaximus.sierra@gmail.com>
2026-05-14 18:44:26 -0700

    Milestone 2

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
 //COMSC-210 | Lab 37 | Alexander Sierra
 #include <iostream>
-//#include "data.txt"
+#include <fstream>
 using namespace std;
 
 int sum_ascii(char []);
 
 int main() {
-    char s[] = "536B9DFC93AF";
+    char s[13];
+    ifstream file("data.txt");
+    int total = 0;
     
-    cout << sum_ascii(s) << endl;
+    while (file >> s) {
+        total += sum_ascii(s);
+    }
+
+    cout << total << endl;
 
     return 0;
 }
-/*
-These targets are present in the dataset and can be used for testing:
-536B9DFC93AF
-1DA9D64D02A0
-666D109AA22E
-E1D2665B21EA
-*/
 
 int sum_ascii(char s[]) {
     int i = 0;
