bf91e4852792a3f9bc4f7eebe286a090c4d44da3
Theshakyhorse <amaximus.sierra@gmail.com>
2026-05-14 18:15:47 -0700

    1st

--- main.cpp
+++ main.cpp
@@ -2,7 +2,33 @@
 #include <iostream>
 using namespace std;
 
+int sum_ascii(char []);
+
 int main() {
+    char txt[13];
+    char a = 'A';
+    cout << a << endl;
+    cout << (int) a << endl;
+    int b = 66;
+    cout << b << endl;
+    cout << (char) b << endl;
+
+    cout << sum_ascii(txt) << endl;
 
     return 0;
+}
+/*
+These targets are present in the dataset and can be used for testing:
+536B9DFC93AF
+1DA9D64D02A0
+666D109AA22E
+E1D2665B21EA
+*/
+
+int sum_ascii(char s[]) {
+    int i = 0;
+    int sum = 0;
+    while (i != 12) {
+        sum += (int) s[i];
+    }
 }
