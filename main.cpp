e2813bea9abb9376ddf579235403069ba2d01e1e
Theshakyhorse <amaximus.sierra@gmail.com>
2026-05-14 19:22:54 -0700

    Milestone 3

--- main.cpp
+++ main.cpp
@@ -8,27 +8,35 @@ using namespace std;
 int gen_hash_index(char []);
 
 int main() {
+    //declarations
     char s[13];
     ifstream file("data.txt");
     int total = 0;
     int index;
     map<int, list<char*>> hash_table;
     
+    //reading data
     while (file >> s) {
         index = gen_hash_index(s);
         hash_table[index].push_back(s);
     }
 
+    //outputs 1st 100 map entries
+    int i = 0;
     for (auto pair : hash_table) {
-        cout << pair.first << ": ";
-        for (auto s : pair.second)
-            cout << s << " ";
-        cout << endl;
+        if (i < 100) {
+            cout << pair.first << ": ";
+            for (auto& txt : pair.second)
+                cout << txt << " ";
+            cout << endl;
+            i++;
+        }
     }
 
     return 0;
 }
 
+//sums then returns a hash index
 int gen_hash_index(char s[]) {
     int i = 0;
     int sum = 0;
