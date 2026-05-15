f0de8a957a3264b1d0738340a89dc654201c4be2
Theshakyhorse <amaximus.sierra@gmail.com>
2026-05-14 19:10:28 -0700

    5th

--- main.cpp
+++ main.cpp
@@ -5,35 +5,36 @@
 #include <list>
 using namespace std;
 
-int sum_ascii(char []);
-int hashFunc(char []);
+int gen_hash_index(char []);
 
 int main() {
     char s[13];
     ifstream file("data.txt");
     int total = 0;
     int index;
-    map<int, list<char []>> hash_table;
+    map<int, list<char*>> hash_table;
     
     while (file >> s) {
-        index = hashFunc(s);
+        index = gen_hash_index(s);
+        hash_table[index].push_back(s);
     }
 
-    cout << total << endl;
+    for (auto pair : hash_table) {
+        cout << pair.first << ": ";
+        for (auto s : pair.second)
+            cout << s << " ";
+        cout << endl;
+    }
 
     return 0;
 }
 
-int sum_ascii(char s[]) {
+int gen_hash_index(char s[]) {
     int i = 0;
     int sum = 0;
     while (i != 12) {
         sum += (int) s[i];
         i++;
     }
-    return sum;
-}
-
-int hashFunc(char s[]) {
-    return sum_ascii(s) % 97;
+    return sum % 97;
 }
